7a65aa3c73a86cd506f7068bb771ab0103961939
armelBla <110736523+armelBla@users.noreply.github.com>
2023-11-20 12:29:57 -0500

    A simple program with Python for Band name

---
+++ PYTHON/1_Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band name generator")
+city_name = input("What's name of the city you grew up in?!\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city_name + " " +pet_name
+print(f"Your band name could be {band_name}")
