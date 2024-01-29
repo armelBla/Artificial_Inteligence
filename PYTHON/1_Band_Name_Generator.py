c837ea78c2dbcb60b53f7546600140d0cf6d5fbc
Armel_TechAlchemy <110736523+armelBla@users.noreply.github.com>
2024-01-29 07:56:22 -0500

    Update 1_Band_Name_Generator.py

--- PYTHON/1_Band_Name_Generator.py
+++ PYTHON/1_Band_Name_Generator.py
@@ -1,5 +1,5 @@
-print("Welcome to the Band name generator")
-city_name = input("What's name of the city you grew up in?!\n")
-pet_name = input("What's your pet's name?\n")
-band_name = city_name + " " +pet_name
-print(f"Your band name could be {band_name}")
+print("Welcome to the Band Name Generator")
+name_of_city = input("what\'s name of the city you grew up in?\n")
+pet_name = input("what\'s your pet\'s name?\n")
+band_name = name_of_city +" "+ pet_name
+print("Your band name could be\n"+ band_name)
