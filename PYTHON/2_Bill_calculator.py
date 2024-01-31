77cbeffd2bad2867ee510b194edba6d5aaf92d95
Armel_TechAlchemy <110736523+armelBla@users.noreply.github.com>
2024-01-31 08:30:26 -0500

    Update 2_Bill_calculator.py

--- PYTHON/2_Bill_calculator.py
+++ PYTHON/2_Bill_calculator.py
@@ -1,9 +1,11 @@
-print("Welcome to the tip calculator.")
-total_bill = float(input("What was the total bill?"))
-percentage_tip = int(input("what percentage tip would you like to give?")) / 100
-total_people = int(input("How many people to split the bill?"))
+print("Welcome to the tip calculator")
+total_bill = float(input("What was the total bill? $\n"))
+percentage_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? \n"))
+number_people = int(input("How many people to split the bill? \n"))
 
-total_tip = total_bill * percentage_tip
-amount_to_pay = round((total_tip + total_bill) / total_people, 2)
+percentage_give = (total_bill * percentage_tip) / 100
+total_amount = total_bill + percentage_give
+amount_to_pay = round((total_amount / number_people), 2)
 
-print(f" Each person should pay: {amount_to_pay}")
+
+print(f"Each person should pay: ${amount_to_pay}")
